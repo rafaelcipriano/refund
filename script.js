3859dd586fc19a38a974cdd05d5b8386ed72e094
rafael <rafaelciprianosilva0@gmail.com>
2024-08-28 16:32:24 -0300

    formatting the currency

---
+++ script.js
@@ -0,0 +1,27 @@
+// Seleciona os elementos do formulário.
+const amount = document.getElementById('amount')
+
+// Captura o evento de input para formatar o valor.
+amount.oninput = () => {
+
+  // Obtém o valor atual do input e remove os caracteres não númericos.
+  let value = amount.value.replace(/\D/g, '')
+
+  /* Transformar o valor em centavos.
+    Exemplo: 150/1500 = 1.5 que é equivalente a R$ 1,50.
+  */
+  value = Number(value) / 100
+
+  // Atualiza o valor do input.
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no formato BRL (Real Brasileiro)
+  value = value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+
+  return value
+}
