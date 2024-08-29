95263daeddab91a4fee636039c5cfbb59ff45ad0
rafael <rafaelciprianosilva0@gmail.com>
2024-08-29 12:31:09 -0300

    remove an item from the list

--- script.js
+++ script.js
@@ -154,3 +154,18 @@ function updateTotals() {
     alert('Não foi possível atualizar os totais')
   }
 }
+
+// Evento que captura o clique nos itens na lista
+expenseList.addEventListener('click', (event) => {
+  // Verifica se o elemento clicado é o ícone de remover
+  if (event.target.classList.contains('remove-icon')) {
+    // Obtém a li pai do elemento clicado
+    const item = event.target.closest('.expense')
+
+    // Remove o item da lista.
+    item.remove()
+  }
+
+  // Atualiza os totais.
+  updateTotals()
+})
